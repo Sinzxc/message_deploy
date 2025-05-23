a5f60262b08bb02ea82a52906cc586084fb37f75
Sinzxc <nik.orig@mail.ru>
2025-05-23 13:01:49 +0300

    updated submodules

--- start_all_apps.sh
+++ start_all_apps.sh
@@ -7,7 +7,12 @@ if ! command -v screen &> /dev/null; then
     exit 1
 fi
 
+# Завершаем существующие сессии с именем "van_apps"
+echo "Checking for existing sessions..."
+screen -ls | grep van_apps | cut -d. -f1 | xargs -I{} screen -S {} -X quit 2>/dev/null
+
 # Создаём новую сессию screen с именем "van_apps"
+echo "Creating new session..."
 screen -dmS van_apps
 
 # Окно 0: Frontend
@@ -21,4 +26,7 @@ screen -S van_apps -X screen -t "FileStorage" bash -c 'cd FileStorageReborn && n
 
 echo "All applications have been started in screen session 'van_apps'!"
 echo "To attach to the session, run: screen -r van_apps"
+echo "To switch between windows: Ctrl+A followed by number (0, 1, 2)"
+echo "To detach from screen: Ctrl+A followed by D"
+echo "To terminate the session, run: screen -S van_apps -X quit"
 read -p "Press Enter to continue..."
